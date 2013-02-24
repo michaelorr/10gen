e938d92a291c1986deb51de84043efff446bc170
Michael Orr <michael@orr.co>
2013-02-24 13:48:11 -0500

    Getting started

---
+++ hello.py
@@ -0,0 +1,19 @@
+import bottle
+import pymongo
+
+#this is the handler for the root address of the web server
+@bottle.route('/')
+def index():
+  from pymongo import Connection
+  connection = Connection('localhost', 27017) # get a connection to the database server
+
+  db = connection.test  # attach to the test database
+
+  names = db.names  # get a handle for the names collection
+
+  item = names.find_one()  # find a single item from names
+
+  return '<b>Hello %s!</b>' % item['name']
+
+
+bottle.run(host='localhost', port=8080)
